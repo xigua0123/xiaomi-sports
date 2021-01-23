755932fd9492ec701c9332a25d2ebd860f35de1f
FateLight <57248740+FKPYW@users.noreply.github.com>
2021-01-23 16:14:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ def main(user, passwd, step):
         return
     
     if step == '':
-        print ("已设置为随机步数（20000-25000）")
-        step = str(random.randint(20000,25000))
+        print ("已设置为随机步数（10000-19999）")
+        step = str(random.randint(10000,19999))
     login_token = 0
     login_token,userid = login(user,password)
     if login_token == 0:
